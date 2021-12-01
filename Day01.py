90b33f01c380105a2e840f0a5703c768b24c202c
Matias I. Bofarull Oddo <moddo@eoas.ubc.ca>
2021-12-01 09:05:06 -0800

    Update Day01.py

--- Day01.py
+++ Day01.py
@@ -22,4 +22,41 @@ for i in range(len(list)):
     if diff < 0:
         count += 1
 
-print(count)
+print("\nTotal count:", count, "\n")
+
+# PART TWO
+
+import os
+
+scriptsPath = os.path.dirname(os.path.realpath(__file__))
+os.chdir(scriptsPath)
+print("\nCurrent working directory: " + str(os.getcwd()) + "\n")
+
+rawFile = open("input.txt")
+contents = rawFile.read()
+data = contents.splitlines()
+
+count = 0
+
+sumList = []
+iteration = -1
+
+for i in range(len(data)):
+    if i < 2:
+        continue
+    iteration += 1
+    sum = int(data[i - 2]) + int(data[i - 1]) + int(data[i])
+    sumList.append(sum)
+
+    print(data[i], end=" ")
+    print(data[i - 1], end=" ")
+    print(data[i - 2], "---> Sum:", sum)
+
+    print(sumList[iteration])
+
+    diff = sumList[iteration - 1] - sumList[iteration]
+
+    if diff < 0:
+        count += 1
+
+print("\nTotal count:", count, "\n")
